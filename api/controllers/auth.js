37d9728cef7e1500d218a7423370bfd277295f1e
AathithiyanS <145222231+AathithiyanS@users.noreply.github.com>
2025-06-17 13:58:06 +0530

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,47 +3,53 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { createError } from "../utils/error.js";
 
+// REGISTER FUNCTION
 export const register = async (req, res, next) => {
+  try {
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(req.body.password, saltRounds);
 
-    try {
-        
-        const saltRounds = 10;
-        const hash = await bcrypt.hash(req.body.password, saltRounds);
-        
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hash,
-        })
-        await newUser.save();
-        res.status(201).send("User has been created");
-    } catch (err) {
-        next(err);
-    }
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
+
+    await newUser.save();
+    res.status(201).send("User has been created");
+  } catch (err) {
+    next(err);
+  }
 };
 
+// LOGIN FUNCTION
 export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found!"));
 
-    try {
-        const user = await User.findOne({ username: req.body.username });
-        if (!user) {
-            return next(createError(404, "User not found!"));
-        }
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordCorrect)
+      return next(createError(400, "Wrong password or username!"));
 
-        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
-        if (!isPasswordCorrect) {
-            return next(createError(400, "Wrong password or username!"));
-        }
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
 
-        const token=jwt.sign({id: user.id,isAdmin: user.isAdmin},process.env.JWT);
+    const { password, isAdmin, ...otherDetails } = user._doc;
 
-        const { password, isAdmin, ...otherDetails } = user._doc;  //Destructuring the User
+    // ✅ Deployment-safe cookie
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: true,        // ✅ Ensures cookie only sent over HTTPS (Render = HTTPS)
+        sameSite: "None",    // ✅ Allows cross-site cookie (frontend & backend are on different domains)
+      })
+      .status(200)
+      .json({ ...otherDetails });
 
-        res.cookie("access_token",token,{httpOnly:true})
-        .status(200)
-        .json({ ...otherDetails });
-        
-    } catch (err) {
-        next(err);
-    }
-}
+  } catch (err) {
+    next(err);
+  }
+};
